f85f3341ee19389ca88c6ad4488dcabf00c64d15
Patrick Armengol <patrickarmengol@protonmail.com>
2023-01-11 11:02:41 +0000

    using vue3

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue';
 
 import './assets/main.css';
 
-Vue.prototype.$apiUrl = process.env.VUE_APP_API_URL || 'http://0.0.0.0/5000';
 
 createApp(App).mount('#app')
+app.config.globalProperties.$apiUrl = process.env.VUE_APP_API_URL || 'http://0.0.0.0/5000';
