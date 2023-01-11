21c977d369aa9beccdb67c47adad62739d9fbb4b
Patrick Armengol <patrickarmengol@protonmail.com>
2023-01-11 11:04:59 +0000

    renamed env variable

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,4 +5,4 @@ import './assets/main.css';
 
 
 const app = createApp(App).mount('#app');
-app.config.globalProperties.$apiUrl = process.env.VUE_APP_API_URL || 'http://0.0.0.0/5000';
+app.config.globalProperties.$apiUrl = process.env.API_URL || 'http://0.0.0.0/5000';
